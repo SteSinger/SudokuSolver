4f137f3964a9edbd3478eb8a3454c9b138280aad
Stephan Singer <singer.stephan@googlemail.com>
2015-01-19 20:38:05 +0100

    Identify unique numbers

--- solver.js
+++ solver.js
@@ -1,66 +1,34 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
 /**
  * Created by Stephael on 11.01.2015.
  */
 
-var steps = new Array();
+var steps = [];
 var currStep = 0;
-function solve(){
-    steps = new Array();
-    parseInput();
 
-    //fill with values
-    for(var i=0; i<9; i++)
-    {
-        for(var j=0; j<9; j++)
-        {
-            if(steps[0][i][j].length == 0)
-            {
-                steps[0][i][j] = [1,2,3,4,5,6,7,8,9];
-            }
+function tableCreate(arr) {
+    "use strict";
+    var td,
+        tr,
+        body = document.body,
+        tbl = document.createElement('table'),
+        i,
+        j;
+
+    for (i = 0; i < 9; i += 1) {
+        tr = tbl.insertRow();
+        for (j = 0; j < 9; j += 1) {
+            td = tr.insertCell();
+            td.innerHTML = parseLine(arr[i][j]);
         }
     }
+    body.appendChild(tbl);
+}
 
-    var changed=true;
-    var step = 0;
-    while(changed)
-    {
-        changed = false;
-
-        for(var i=0; i<9; i++)
-        {
-            for(var j=0; j<9; j++)
-            {
-                var length = steps[step][i][j].length;
-                if(length > 1)
-                {
-                    steps[step][i][j] = steps[step][i][j].filter(
-                        function(element)
-                        {
-                            var inRow = valuesInRow(i, steps[step]).indexOf(element);
-                            var inCol = valuesInCol(j, steps[step]).indexOf(element);
-                            var inCell = valuesInCell(Math.floor(j/3) +3*Math.floor(i/3), steps[step]).indexOf(element);
-                            return inRow == -1 && inCol == -1 && inCell == -1;
-                        }
-                    );
-                    if(length != steps[step][i][j].length)
-                    {
-                        changed = true;
-                    }
-                    if(steps[step][i][j].length == 1)
-                    {
-                        var copyArray = steps[step];
-                        steps[step + 1] = JSON.parse(JSON.stringify(copyArray));
-                        step += 1;
-                    }
-                }
-            }
-        }
-    }
-
-    tableCreation();
-};
-
-function tableCreation() {
+function deleteTableAndCreateNew() {
+    "use strict";
     $("#curr").text(currStep + 1);
     $("#total").text(steps.length);
 
@@ -68,151 +36,130 @@ function tableCreation() {
     tableCreate(steps[currStep]);
 }
 
-function first()
-{
-    if(steps.length == 0)
-        return;
-    currStep = 0;
-    tableCreation();
+function getCell(row, col) {
+    "use strict";
+    return Math.floor(col / 3) + 3 * Math.floor(row / 3);
 }
 
-function next()
-{
-    if(steps.length == 0)
-        return;
-    currStep = Math.min(currStep + 1, steps.length - 1);
-    tableCreation();
-}
+function countFree(row, col, arr) {
+    "use strict";
+    var offsetCol,
+        offsetRow,
+        cell,
+        values = new Array(3),
+        i,
+        j;
 
-function prev()
-{
-    if(steps.length == 0)
-        return;
-    currStep = Math.max(currStep - 1, 0);
-    tableCreation();
-}
-
-function last()
-{
-    if(steps.length == 0)
-        return;
-    currStep = steps.length-1;
-    tableCreation();
-}
-
-
-function tableCreate(arr){
-    var body = document.body;
-    var tbl  = document.createElement('table');
-    tbl.style.border = "1px solid black";
-    tbl.style.textAlign = "center";
-    tbl.style.borderCollapse = "collapse";
-    for(var i = 0; i < 9; i++){
-        var tr = tbl.insertRow();
-        for(var j = 0; j < 9; j++){
-            var td = tr.insertCell();
-            td.innerHTML = parseLine(arr[i][j]);
-            //td.appendChild(document.createTextNode(parseLine(arr[i][j])));
-            td.style.border = "1px solid black";
-            td.style.width = "60px";
-            td.style.height = "60px";
+    for (i = 0; i < values.length; i += 1) {
+        values[i] = new Array(10);
+        for (j = 0; j < values[i].length; j += 1) {
+            values[i][j] = 0;
         }
     }
-    body.appendChild(tbl);
-}
 
-function parseLine(arr)
-{
-    var result = "";
-    for(var i=0; i < arr.length; i++)
-    {
-        result += arr[i];
-        if(i%3 == 2 && i>0)
-        {
-            result += "<br/>";
-        }else if(i < arr.length-1)
-        {
-            result += ",";
-        }
-
-
-    }
-    return result;
-}
-
-function isSolved(){
-    var solved = false;
-
-
-
-    solved = true;
-    return solved;
-}
-
-
-function checkRow(row) {
-
-    var values = new Array(10);
-
-    for(var i = 0; i<9; i++)
-    {
-        var fieldVal = steps[0][row][i];
-
-        if(fieldVal.length == 1)
-        {
-            if(values[fieldVal[0]] == null)
-            {
-                values[fieldVal[0]] = true;
+    for (i = 0; i < 9; i += 1) {
+        arr[row][i].forEach(
+            function (currentValue, index, array) {
+                if (array.length > 1) {
+                    values[0][currentValue] += 1;
+                }
             }
-            else
-            {
+        );
+        arr[i][col].forEach(
+            function (currentValue, index, array) {
+                if (array.length > 1) {
+                    values[1][currentValue] += 1;
+                }
+            }
+        );
+    }
+    for (i = 0; i < 3; i += 1) {
+        for (j = 0; j < 3; j += 1) {
+
+            cell = getCell(row, col);
+            offsetRow = 3 * Math.floor(cell / 3);
+            offsetCol = 3 * (cell % 3);
+            arr[i + offsetRow][j + offsetCol].forEach(function (currentValue, index, array) {
+                if (array.length > 1) {
+                    values[2][currentValue] += 1;
+                }
+            });
+        }
+    }
+    return values;
+}
+
+// Validations
+
+function validateRow(row) {
+    "use strict";
+    var fieldVal,
+        values = new Array(10),
+        i;
+
+    for (i = 0; i < values.length; i += 1) {
+        values[i] = false;
+    }
+
+    for (i = 0; i < 9; i += 1) {
+        fieldVal = steps[0][row][i];
+
+        if (fieldVal.length === 1) {
+            if (values[fieldVal[0]] === false) {
+                values[fieldVal[0]] = true;
+            } else {
                 alert("Die " + fieldVal[0] + " ist doppelt Eingetragen in Zeile " + (row + 1));
             }
         }
     }
 }
 
-function checkColumn(col) {
-    var values = new Array(10);
+function validateColumn(col) {
+    "use strict";
+    var values = new Array(10),
+        i,
+        fieldVal;
 
-    for(var i = 0; i<9; i++)
-    {
-        var fieldVal = steps[0][i][col];
+    for (i = 0; i < values.length; i += 1) {
+        values[i] = false;
+    }
 
-        if(fieldVal.length == 1)
-        {
-            if(values[fieldVal[0]] == null)
-            {
+    for (i = 0; i < 9; i += 1) {
+        fieldVal = steps[0][i][col];
+
+        if (fieldVal.length === 1) {
+            if (values[fieldVal[0]] === false) {
                 values[fieldVal[0]] = true;
-            }
-            else
-            {
+            } else {
                 alert("Die " + fieldVal[0] + " ist doppelt Eingetragen in Spalte " + (col + 1));
             }
         }
     }
 }
 
-function checkCell(cell) {
+function validateCell(cell) {
+    "use strict";
+    var values = new Array(10),
+        i,
+        j,
+        offsetRow,
+        offsetCol,
+        fieldVal;
 
-    var values = new Array(10);
+    for (i = 0; i < values.length; i += 1) {
+        values[i] = false;
+    }
 
-    for(var i = 0; i<3; i++)
-    {
-        for(var j = 0; j<3; j++)
-        {
-            var offsetRow = 3*Math.floor(cell/3);
-            var offsetCol = 3*(cell%3);
-            var fieldVal = steps[0][i + offsetRow][j + offsetCol];
+    for (i = 0; i < 3; i += 1) {
+        for (j = 0; j < 3; j += 1) {
+            offsetRow = 3 * Math.floor(cell / 3);
+            offsetCol = 3 * (cell % 3);
+            fieldVal = steps[0][i + offsetRow][j + offsetCol];
 
-            if(fieldVal.length == 1)
-            {
-                if(values[fieldVal[0]] == null)
-                {
+            if (fieldVal.length === 1) {
+                if (values[fieldVal[0]] === false) {
                     values[fieldVal[0]] = true;
-                }
-                else
-                {
+                } else {
                     alert("Die " + fieldVal[0] + " ist doppelt Eingetragen in Zelle " + (cell + 1));
                 }
             }
@@ -220,19 +167,23 @@ function checkCell(cell) {
     }
 }
 
+
 function valuesInCell(cell, arr) {
-    var values = new Array(9);
+    "use strict";
+    var values = new Array(9),
+        i,
+        j,
+        offsetRow,
+        offsetCol,
+        fieldVal;
 
-    for(var i = 0; i<3; i++)
-    {
-        for(var j = 0; j<3; j++)
-        {
-            var offsetRow = 3*Math.floor(cell/3);
-            var offsetCol = 3*(cell%3);
-            var fieldVal = arr[i + offsetRow][j + offsetCol];
+    for (i = 0; i < 3; i += 1) {
+        for (j = 0; j < 3; j += 1) {
+            offsetRow = 3 * Math.floor(cell / 3);
+            offsetCol = 3 * (cell % 3);
+            fieldVal = arr[i + offsetRow][j + offsetCol];
 
-            if(fieldVal.length == 1)
-            {
+            if (fieldVal.length === 1) {
                 values[fieldVal[0]] = fieldVal[0];
             }
         }
@@ -241,14 +192,15 @@ function valuesInCell(cell, arr) {
 }
 
 function valuesInRow(row, arr) {
-    var values = new Array(9);
+    "use strict";
+    var values = new Array(9),
+        i,
+        fieldVal;
 
-    for(var i = 0; i<9; i++)
-    {
-        var fieldVal = arr[row][i];
+    for (i = 0; i < 9; i += 1) {
+        fieldVal = arr[row][i];
 
-        if(fieldVal.length == 1)
-        {
+        if (fieldVal.length === 1) {
             values[fieldVal[0]] = fieldVal[0];
         }
     }
@@ -256,55 +208,240 @@ function valuesInRow(row, arr) {
 }
 
 function valuesInCol(col, arr) {
+    "use strict";
+    var values = new Array(9),
+        i,
+        fieldVal;
 
-    var values = new Array(9);
+    for (i = 0; i < 9; i += 1) {
+        fieldVal = arr[i][col];
 
-    for(var i = 0; i<9; i++)
-    {
-        var fieldVal = arr[i][col];
-
-        if(fieldVal.length == 1)
-        {
+        if (fieldVal.length === 1) {
             values[fieldVal[0]] = fieldVal[0];
         }
     }
     return values;
 }
 
-function parseInput(){
-    var input = new Array();
-    var line;
-    $("input").each(function(index)
-    {
-        if(index % 9 == 0)
-        {
-            line = new Array();
+function parseLine(arr) {
+    "use strict";
+    var result = "",
+        i;
+    for (i = 0; i < arr.length; i += 1) {
+        result += arr[i];
+        if (i % 3 === 2 && i > 0) {
+            result += "<br/>";
+        } else if (i < arr.length - 1) {
+            result += ",";
+        }
+    }
+    return result;
+}
+
+function parseInput() {
+    "use strict";
+    var input = [],
+        line,
+        i;
+    $("input").each(function (index) {
+        if (index % 9 === 0) {
+            line = [];
             input.push(line);
         }
         var entry = $(this).val();
-        if(entry != "")
-        {
-            line.push([parseInt(entry)]);
-        }
-        else
-        {
+        if (entry !== "") {
+            line.push([parseInt(entry, 10)]);
+        } else {
             line.push([]);
         }
     });
     steps[0] = input;
 
-    for(var i = 0; i<9; i++)
-    {
-        checkRow(i);
-        checkColumn(i);
-        checkCell(i);
+    for (i = 0; i < 9; i += 1) {
+        validateRow(i);
+        validateColumn(i);
+        validateCell(i);
     }
-};
+}
 
+function isInRow(element, row, arr) {
+    "use strict";
+    return valuesInRow(row, arr).indexOf(element) !== -1;
+}
 
+function isInColumn(element, column, arr) {
+    "use strict";
+    return valuesInCol(column, arr).indexOf(element) !== -1;
+}
 
-if (!Array.prototype.last){
-    Array.prototype.last = function(){
+function isInCell(element, cell, arr) {
+    "use strict";
+    return valuesInCell(cell, arr).indexOf(element) !== -1;
+}
+
+// called from html
+
+function solve() {
+    "use strict";
+    var nums,
+        copyArray,
+        length,
+        i,
+        j,
+        changed = true,
+        step = 0;
+    steps = [];
+    parseInput();
+
+    //fill with default values
+    for (i = 0; i < 9; i += 1) {
+        for (j = 0; j < 9; j += 1) {
+            if (steps[0][i][j].length === 0) {
+                steps[0][i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            }
+        }
+    }
+
+    while (changed) {
+        changed = false;
+
+        for (i = 0; i < 9; i += 1) {
+            for (j = 0; j < 9; j += 1) {
+                length = steps[step][i][j].length;
+                if (length > 1) {
+                    steps[step][i][j] = steps[step][i][j].filter(
+                        function (element) {
+                            var inRow = isInRow(element, i, steps[step]),
+                                inCol = isInColumn(element, j, steps[step]),
+                                inCell = isInCell(element, getCell(i, j), steps[step]);
+                            return !inRow  && !inCol && !inCell;
+                        }
+                    );
+                    if (length !== steps[step][i][j].length) {
+                        changed = true;
+                    }
+                    if (steps[step][i][j].length === 1) {
+                        copyArray = steps[step];
+                        step += 1;
+                        steps[step] = JSON.parse(JSON.stringify(copyArray));
+                    }
+                }
+            }
+        }
+
+        if (changed === false) {
+            for (i = 0; i < 9; i += 1) {
+                for (j = 0; j < 9; j += 1) {
+                    if (steps[step][i][j].length !== 1) {
+                        nums = countFree(i, j, steps[step]);
+
+                        nums.forEach(function (value, index, array) {
+                            var innerCount = 0,
+                                innerValues = [];
+                            value.forEach(function (val, ind) {
+
+                                if (val === 1) {
+                                    if (steps[step][i][j].indexOf(ind) !== -1) {
+                                        innerCount += 1;
+                                        innerValues.push(ind);
+                                    }
+                                }
+                            });
+                            if (innerCount === 1) {
+                                changed = true;
+                                steps[step][i][j] = innerValues;
+                                copyArray = steps[step];
+                                step += 1;
+                                steps[step] = JSON.parse(JSON.stringify(copyArray));
+                            }
+                        });
+                    }
+                    if (changed) {
+                        break;
+                    }
+                }
+                if (changed) {
+                    break;
+                }
+            }
+        }
+    }
+    deleteTableAndCreateNew();
+}
+
+function reset() {
+    "use strict";
+    steps = [];
+    currStep = 0;
+
+    $("#curr").text(0);
+    $("#total").text(0);
+
+    $("table").remove();
+    $("input").each(function () {
+        $(this).val("");
+    });
+}
+
+function displayFirstStep() {
+    "use strict";
+    if (steps.length === 0) {
+        return;
+    }
+    currStep = 0;
+    deleteTableAndCreateNew();
+}
+
+function displayNextStep() {
+    "use strict";
+    if (steps.length === 0) {
+        return;
+    }
+    currStep = Math.min(currStep + 1, steps.length - 1);
+    deleteTableAndCreateNew();
+}
+
+function displayPreviousStep() {
+    "use strict";
+    if (steps.length === 0) {
+        return;
+    }
+    currStep = Math.max(currStep - 1, 0);
+    deleteTableAndCreateNew();
+}
+
+function displayLastStep() {
+    "use strict";
+    if (steps.length === 0) {
+        return;
+    }
+    currStep = steps.length - 1;
+    deleteTableAndCreateNew();
+}
+
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        "use strict";
         return this[this.length - 1];
     };
-};
+}
+
+$(document).keydown(function(evt) {
+    "use strict";
+    switch (evt.keyCode) {
+    case 37:    // left
+        displayPreviousStep();
+        break;
+    case 39:    // right
+        displayNextStep();
+        break;
+    case 13:
+        solve();
+        break;
+    case 82:
+        reset();
+        break;
+    default:
+        break;
+    }
+});
